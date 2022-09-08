ca7b25223e30e70839fb37c8f3b69926dab5163c
justice duruanyim <uzochiduru8747@gmail.com>
2022-09-08 21:48:32 +0000

    tenth commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	write(2 "and that piece of art is usefull\" -Dora ろper, 2015-10-19\n", 59);
-return (0);
+	write(2 "and that piece of art is usefull\" -Dora Korper, 2015-10-19\n", 59);
+return (1);
 }
 
