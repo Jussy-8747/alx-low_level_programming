7ec4d1c5d96907befa26e474f16ac77f9ee0ce8f
justice duruanyim <uzochiduru8747@gmail.com>
2022-09-08 21:29:22 +0000

    eleventh commit

---
+++ 101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main program entry point 
+ * Return: one on success
+ */
+int main(void)
+{
+	write(2, "and that piece of art is usefull\" -Dora Korpar 2015-10-19\n"59);
+return (0);
+}
+
