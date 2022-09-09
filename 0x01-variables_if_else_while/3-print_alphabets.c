6465aa169e5fd07fed4f79c2cc33ebd40a62e256
Jussy-8747 <uzochiduru8747@gmail.com>
2022-09-09 08:00:36 -0700

    first commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
- 
-int main()
+/**
+ *main -entry point
+ *Return: 0 if success
+*/
+int main(void)
 {
 char i;
- 
-printf("Capital (upper) case characters:\n");
-for(i='A'; i<='Z'; i++)
-printf("%c ",i);
- 
 printf("\n\nLower case characters:\n");
-for(i='a'; i<='z'; i++)
-printf("%c ",i);
-     
-return 0;
+for (i = 'a'; i <= 'z'; i++)
+printf("%c ", i);
+printf("Capital (upper) case characters:\n");
+for (i = 'A'; i <= 'Z'; i++)
+printf("%c ", i);
+return (0);
 }
