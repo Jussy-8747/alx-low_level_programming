86845cb33cb76c1eff51ae1b8fb8f54711a6fb49
Jussy-8747 <uzochiduru8747@gmail.com>
2022-09-13 09:05:31 -0700

    first commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *main - print alphabet in lowercase
-*/
-
-void print_alphabet(void);
-int main(void
-{
-print_alphabet();
-return (0);
-}
+ *Main- print alphabet in lowercase
+ *
+ *Return: 0
+ */
 
 void print_alphabet(void)
 {
