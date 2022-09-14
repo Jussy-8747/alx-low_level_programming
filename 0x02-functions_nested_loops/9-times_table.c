50e7ffd6567633128180d4206ee6d63cb2435966
Jussy-8747 <uzochiduru8747@gmail.com>
2022-09-14 08:15:41 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,9 +14,12 @@ _putchar(',');
 _putchar(' ');
 prod = num * mult;
 if (prod <= 9)
+{
 _putchar(' ');
+}
 else
-  _putchar((prod / 10) + '0'));
+{
+_putchar((prod / 10) + '0'));
 _putchar((prod % 10) + '0'));
 }
 _putchar('\n');
