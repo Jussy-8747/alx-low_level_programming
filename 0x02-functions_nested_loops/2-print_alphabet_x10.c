e18cfcc85b0c98815d60d2549bd21e264233b4ee
Jussy-8747 <uzochiduru8747@gmail.com>
2022-09-13 19:55:53 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print alphabbet in lowercase 10times
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char leter;
+for (i + 0; i <= 10; i++)
+{
+for (leter = 'a'; leter <= 'z'; leter++)
+{
+_putchar(leter);
+}
+_putchar('\n');
+}
