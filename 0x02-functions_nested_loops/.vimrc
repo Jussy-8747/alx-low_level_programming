8b67bb181b84c077888f2ed823ab5213dc22fc97
Jussy-8747 <uzochiduru8747@gmail.com>
2022-09-14 07:46:38 -0700

    task 7

---
+++ 0x02-functions_nested_loops/.vimrc
@@ -0,0 +1,5 @@
+set autoindent
+set smartindent
+set cindent
+syntax enableset 
+set number
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,4 +13,8 @@ last_digit *= -1;
 _putchar(last_digit + '0');
 return (last_digit);
 }
+else
+{
+return (0);
+}
 }
