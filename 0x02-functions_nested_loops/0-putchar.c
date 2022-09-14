0356421faf48579acd9f645a5484cd9c1e069160
Jussy-8747 <uzochiduru8747@gmail.com>
2022-09-13 22:39:01 -0700

    first task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,12 @@
  */
 int _putchar(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+char text[10] = "_putchar";
+int i;
+for (i = 0; i <= 8; i++)
+{
+_putchar(text[i]);
+}
 _putchar('\n');
 return (0);
 }
