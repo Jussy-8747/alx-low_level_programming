bb15de40abab3ae0b682d658816e16db9e4d0d42
Jussy-8747 <uzochiduru8747@gmail.com>
2022-09-13 22:22:55 -0700

    first task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,18 @@
-#include "main.h" 
-/** main - print putchar
+#include "main.h"
+/**
+ *_putchar - print _putchar
  *Return: 0
- */
-int main(void)
+ *
+int _putchar()
 {
-printf("_putchar");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n);
 return (0);
 }
