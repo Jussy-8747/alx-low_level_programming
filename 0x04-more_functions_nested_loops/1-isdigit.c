985301ee9b7e085754194da6dc740fa1a60a9d54
Jussy-8747 <uzochiduru8747@gmail.com>
2022-09-16 03:28:32 -0700

    task 2

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _isupper - Checks for uppercase characters.
  * @c: The character to be checked.
@@ -8,8 +7,12 @@
  */
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-return (1);
-else
-return (0);
+  if (c >= 'A' && c <= 'Z')
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
 }
