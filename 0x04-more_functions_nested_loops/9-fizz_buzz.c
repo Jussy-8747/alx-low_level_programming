a2a8d2fb5ca0f63937224187e9e8b135fd579fbf
Jussy-8747 <uzochiduru8747@gmail.com>
2022-09-16 02:07:43 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,18 +8,16 @@ int main(void)
 {
 int i;
 for (i = 1; i <= 100; i++)
+{
 if (i % 3 == 0 && i % 5 == 0)
-{
-_putchar(" FizzBuzz"};
+_putcha(" FizzBuzz");
 else if (i % 3 == 0)
-{
 _putchar(" Fizz");
-}
 else if (i % 5 == 0)
-{
 _putchar(" Buzz");
-}
 else
 _putchar(" %d", i);
+}
 return (0);
+}
 }
