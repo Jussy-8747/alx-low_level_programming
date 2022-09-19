e1f6a495b3c4d10e36f8c57e4092c10931f14c5a
Jussy-8747 <uzochiduru8747@gmail.com>
2022-09-19 06:05:39 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *swap_int - swap the value of twk integers
+ *@a: first integer
+ *@b: second integer
+ */
+void swap_int(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+_putchar("%d\n", a);
+_putchar("%d\n", b);
+
+}
