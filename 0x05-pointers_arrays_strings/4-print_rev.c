8c3229ee66c88d15e6a46fa8f9380cdac14688f9
justice duruanyim <uzochiduru8747@gmail.com>
2022-09-19 20:23:35 +0000

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * print_rev - print a string in reverse
  * @s: the string
@@ -6,10 +7,13 @@
 void print_rev(char *s)
 {
 int lent = 0;
-int i = 0;
-while (s[i++])
-lent++;
-for (i = lent - 1; i >= 0; i--)
-_putchar(s[i]);
-_putchar("\n");
+int i;
+char temp;
+int middle;
+lent = strlen(s);
+middle = lent / 2;
+for (i = 0; i < middle; i++)
+temp = s[i];
+s[i] = s[lent - i - 1];
+s[i] = s[lent - i - 1] = temp;
 }
