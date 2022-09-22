9ab7fb885e26fe28b74c17701378563eab5ec0cc
Jussy-8747 <uzochiduru8747@gmail.com>
2022-09-21 21:49:17 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_strcat - concatenates tow strings.
+ *@dest: destination of the strings.
+ *@src: sourse of the string.
+ *Return: return 1.
+ */
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
