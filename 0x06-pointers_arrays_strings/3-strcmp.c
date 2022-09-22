b401f87c5fd7fd1a1d90c646c5579949abc846b8
Jussy-8747 <uzochiduru8747@gmail.com>
2022-09-21 22:27:55 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcmp - compare two strings
+ *@s1: the first string.
+ *@s2: the second string.
+ *Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+strcmp(s1, s2);
+return (0);
+}
