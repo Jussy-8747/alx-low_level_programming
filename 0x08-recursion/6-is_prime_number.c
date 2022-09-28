8c0bad4a55d301ed9979b55fefd6aea2d068b8c6
Jussy-8747 <uzochiduru8747@gmail.com>
2022-09-28 05:58:16 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *is_prime_number - chexks if number is a prime number
+ *@n: the number.
+ *Return: 0 if not prime return 1
+ */
+int is_prime_number(int n)
+{
+if (2 % n == 0)
+return (0);
+else
+return (1);
+}
