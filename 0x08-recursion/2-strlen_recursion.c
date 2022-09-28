29054d48bb2c79f346a48b9e8f749cfa7edd6d0c
Jussy-8747 <uzochiduru8747@gmail.com>
2022-09-28 02:22:41 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<string.h>
+/**
+ *_strlen_recursion - print the length of a string
+ *@s: the string
+ *Return: the lenght of the string
+ */
+int _strlen_recursion(char *s)
+{
+int lent = strlen(s);
+return (lent);
+}
